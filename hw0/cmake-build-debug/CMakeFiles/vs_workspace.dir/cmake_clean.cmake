835106bf7647b4b10f3c529c19fce4fd375222b0
RonFed <ronfederman@mail.tau.ac.il>
2021-04-17 23:49:34 +0300

    creating the repo

---
+++ hw0/cmake-build-debug/CMakeFiles/vs_workspace.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vs_workspace.dir/209339290_211670849_assignment0.c.obj"
+  "libvs_workspace.dll.a"
+  "vs_workspace.exe"
+  "vs_workspace.exe.manifest"
+  "vs_workspace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vs_workspace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
