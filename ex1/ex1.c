073967ccfb5f245c1c7837a2ad4520a78ab2819b
RonFed <ronfederman@mail.tau.ac.il>
2021-04-20 22:44:22 +0300

    added validation checks for command line arguments

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#define BUFFERSIZE 100
+#define BUFFERSIZE 1000
 #define BASE_ARR_SIZE 20
 #define ARR_SIZE_MULTIPLY 2
 #define COMMA ','
 #define MALLOC_FOR_STRING(str) (char *)(malloc((strlen(str) + 1) * sizeof(char)))
 
 int d = 0;
+int k = 0;
+int max_iter = 200;
 void split_line_to_strings_nums(char *str, char ***);
 void count_commas_in_str(char *str, int *d);
 void turn_lines_to_vectors(int *d, int *lines_count, char ***lines, float ***vers);
+int is_positive_int(const char *str);
+void check_command_line_args(int *argc, char const *argv[]);
 
 int main(int argc, char const *argv[])
 {
+    check_command_line_args(&argc, argv);
+
     int lines_count = 0;
     int arrlen = BASE_ARR_SIZE;
     char buffer[BUFFERSIZE];
@@ -39,25 +45,24 @@ int main(int argc, char const *argv[])
         lines_count++;
     }
 
-    float  ** vectors;
+    float **vectors;
     turn_lines_to_vectors(&d, &lines_count, &lines, &vectors);
     free(lines);
     for (int i = 0; i < lines_count; i++)
     {
-       for (int k = 0; k < d; k++)
-       {
-           printf("%.4f ", vectors[i][k]);
-       }
-       printf("\n");
+        for (int k = 0; k < d; k++)
+        {
+            printf("%.4f ", vectors[i][k]);
+        }
+        printf("\n");
     }
-    
     return 0;
 }
 
 void count_commas_in_str(char *str, int *d)
 {
     int count = 0;
-    for (size_t i = 0; i < strlen(str); i++)
+    for (int i = 0; i < strlen(str); i++)
     {
         if (str[i] == COMMA)
         {
@@ -84,7 +89,7 @@ void split_line_to_strings_nums(char *str, char ***result)
 
 void turn_lines_to_vectors(int *d, int *lines_count, char ***lines, float ***vers)
 {
-    (*vers) = (float **)(malloc((*lines_count) * sizeof(float*)));
+    (*vers) = (float **)(malloc((*lines_count) * sizeof(float *)));
     char *data[*lines_count];
     char **current_line = NULL;
     for (int i = 0; i < *lines_count; i++)
@@ -96,6 +101,56 @@ void turn_lines_to_vectors(int *d, int *lines_count, char ***lines, float ***ver
         for (int j = 0; j < *d; j++)
         {
             (*vers)[i][j] = (float)atof(current_line[j]);
+        }
+    }
+}
+
+int is_positive_int(const char *str)
+{
+    char digit = str[0];
+    if (digit <= '0' || digit > '9')
+    {
+        return 0;
+    }
+    for (int i = 1; i < strlen(str); i++)
+    {
+        digit = str[i];
+        if (digit < '0' || digit > '9')
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void check_command_line_args(int *argc, char const *argv[])
+{
+    if (*argc == 1)
+    {
+        printf("k is not provided \n");
+        assert(*argc > 1);
+    }
+    if (!is_positive_int(argv[1]))
+    {
+        printf("k value is not valid \n");
+        assert(is_positive_int(argv[1]));
+    }
+    k = atoi(argv[1]);
+    if (*argc > 3)
+    {
+        printf("Too many arguments \n");
+        assert(*argc <= 2);
+    }
+    if (*argc == 3)
+    {
+        if (!is_positive_int(argv[2]))
+        {
+            printf("max_iter value is not valid \n");
+            assert(is_positive_int(argv[2]));
+        }
+        else
+        {
+            max_iter = atoi(argv[2]);
         }
     }
 }
