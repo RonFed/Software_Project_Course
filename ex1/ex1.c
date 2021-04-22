d5a73e6b4bba79f53ed2c8bbee891655c897ac35
RonFed <ronfederman@mail.tau.ac.il>
2021-04-22 23:08:47 +0300

    first version working still very small diffrences from the formal output1 in C version

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -7,56 +7,121 @@
 #define ARR_SIZE_MULTIPLY 2
 #define COMMA ','
 #define MALLOC_FOR_STRING(str) (char *)(malloc((strlen(str) + 1) * sizeof(char)))
+void updateS(float m[], int s[], int x);
 
 int d = 0;
 int k = 0;
+float minus_one = -1;
 int max_iter = 200;
+int lines_count;
+void read_text_to_lines(char *** lines);
 void split_line_to_strings_nums(char *str, char ***);
 void count_commas_in_str(char *str, int *d);
 void turn_lines_to_vectors(int *d, int *lines_count, char ***lines, float ***vers);
 int is_positive_int(const char *str);
 void check_command_line_args(int *argc, char const *argv[]);
+void distacne(float **a, float **b, float *result);
+void closest_cluster_index(float *vector, float **centers, int *index_result);
+void multiply_scalar(float *vector, float *scalar, float *result);
+void add_vectors(float *a, float *b, float *result);
+void print_final(float **mat);
+void malloc_for_mat_and_set_zeros(float ***mat, int lines, int cols);
 
 int main(int argc, char const *argv[])
 {
     check_command_line_args(&argc, argv);
 
-    int lines_count = 0;
+    char **lines;
+    read_text_to_lines(&lines);
+
+    float **vectors;
+    turn_lines_to_vectors(&d, &lines_count, &lines, &vectors);
+    free(lines);
+
+    float **centers;
+    malloc_for_mat_and_set_zeros(&centers, k, d);
+    float **cluster_sums;
+    malloc_for_mat_and_set_zeros(&cluster_sums, k, d);
+    int cluster_size[k];
+    int which_cluster[lines_count];
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            centers[i][j] = vectors[i][j];
+        }
+        cluster_size[i] = 0;
+    }
+    for (int i = 0; i < lines_count; i++)
+    {
+        which_cluster[i] = -1;
+    }
+    for (int i = 0; i < max_iter; i++)
+    {
+        int differ = 0;
+        for (int j = 0; j < lines_count; j++)
+        {
+            int new_closest_cluster;
+            closest_cluster_index(vectors[j], centers, &new_closest_cluster);
+            if (new_closest_cluster != which_cluster[j])
+            {
+                if (which_cluster[j] != -1)
+                {
+                    cluster_size[which_cluster[j]] -= 1;
+                    float *negative_vec;
+                    negative_vec = (float *)malloc(d * sizeof(float));
+                    multiply_scalar(vectors[j], &minus_one, negative_vec);
+                    add_vectors(cluster_sums[which_cluster[j]], negative_vec, cluster_sums[which_cluster[j]]);
+                }
+                cluster_size[new_closest_cluster] += 1;
+                add_vectors(cluster_sums[new_closest_cluster], vectors[j], cluster_sums[new_closest_cluster]);
+                which_cluster[j] = new_closest_cluster;
+                differ = 1;
+            }
+        }
+        if (differ == 0)
+        {
+            break;
+        }
+        for (int l = 0; l < k; l++)
+        {
+            if (cluster_size[l] != 0)
+            {
+                float inverse_size;
+                inverse_size = (float) 1 / cluster_size[l];
+                multiply_scalar(cluster_sums[l], &inverse_size, centers[l]);
+            }
+        }
+    }
+    print_final(centers);
+
+    return 0;
+}
+
+void read_text_to_lines(char *** lines) {
+    lines_count = 0;
     int arrlen = BASE_ARR_SIZE;
     char buffer[BUFFERSIZE];
-    char **lines = (char **)(malloc(arrlen * sizeof(char *)));
-    assert(lines != NULL);
+    *lines = (char **)(malloc(arrlen * sizeof(char *)));
+    assert((*lines) != NULL);
     while (fgets(buffer, BUFFERSIZE, stdin) != NULL)
     {
         if (lines_count == arrlen)
         {
             arrlen *= ARR_SIZE_MULTIPLY;
-            lines = (char **)(realloc(lines, arrlen * sizeof(char *)));
-            assert(lines != NULL);
+            *lines = (char **)(realloc(*lines, arrlen * sizeof(char *)));
+            assert(*lines != NULL);
         }
         if (d == 0)
         {
             count_commas_in_str(buffer, &d);
         }
 
-        lines[lines_count] = (char *)(malloc((strlen(buffer) + 1) * sizeof(char)));
-        assert(lines[lines_count] != NULL);
-        strcpy(lines[lines_count], buffer);
-        lines_count++;
+        (*lines)[lines_count] = (char *)(malloc((strlen(buffer) + 1) * sizeof(char)));
+        assert((*lines)[lines_count] != NULL);
+        strcpy((*lines)[lines_count], buffer);
+        (lines_count)++;
     }
-
-    float **vectors;
-    turn_lines_to_vectors(&d, &lines_count, &lines, &vectors);
-    free(lines);
-    for (int i = 0; i < lines_count; i++)
-    {
-        for (int k = 0; k < d; k++)
-        {
-            printf("%.4f ", vectors[i][k]);
-        }
-        printf("\n");
-    }
-    return 0;
 }
 
 void count_commas_in_str(char *str, int *d)
@@ -152,5 +217,76 @@ void check_command_line_args(int *argc, char const *argv[])
         {
             max_iter = atoi(argv[2]);
         }
+    }
+}
+
+void distacne(float **a, float **b, float *result)
+{
+    float sum = 0;
+    for (int i = 0; i < d; i++)
+    {
+        sum += ((*a)[i] - (*b)[i]) * ((*a)[i] - (*b)[i]);
+    }
+    *result = sum;
+}
+
+void closest_cluster_index(float *vector, float **centers, int *index_result)
+{
+    int min_dist_index = 0;
+    float min_dist;
+    distacne(&vector, &centers[0], &min_dist);
+    for (int i = 0; i < k; i++)
+    {
+        float current_dist;
+        distacne(&vector, &centers[i], &current_dist);
+        if (current_dist < min_dist)
+        {
+            min_dist = current_dist;
+            min_dist_index = i;
+        }
+    }
+    *index_result = min_dist_index;
+}
+
+void multiply_scalar(float *vector, float *scalar, float *result)
+{
+    for (int i = 0; i < d; i++)
+    {
+        result[i] = vector[i] * (*scalar);
+    }
+}
+
+void add_vectors(float *a, float *b, float *result)
+{
+    for (int i = 0; i < d; i++)
+    {
+        result[i] = a[i] + b[i];
+    }
+}
+
+void print_final(float **mat)
+{
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < d - 1; j++)
+        {
+            printf("%.4f,", mat[i][j]);
+        }
+        printf("%.4f", mat[i][d - 1]);
+        printf("\n");
+    }
+}
+
+void malloc_for_mat_and_set_zeros(float ***mat, int lines, int cols)
+{
+    *mat = (float **)malloc(lines * sizeof(float *));
+    for (int i = 0; i < lines; i++)
+    {
+        (*mat)[i] = (float *)malloc(cols * sizeof(float));
+        for (int j = 0; j < cols; j++)
+        {
+            (*mat)[i][j] = 0;
+        }
+        
     }
 }
--- ex1/mytest.bat
+++ ex1/mytest.bat
@@ -1,8 +1,16 @@
-python ex1.py 3 600 < tests/input_1.txt > my_output1.txt
-python ex1.py 7 < tests/input_2.txt > my_output2.txt
-python ex1.py 15 300 < tests/input_3.txt > my_output3.txt
+python ex1.py 3 600 < tests/input_1.txt > my_output1py.txt
+python ex1.py 7 < tests/input_2.txt > my_output2py.txt
+python ex1.py 15 300 < tests/input_3.txt > my_output3py.txt
 
-FC my_output1.txt tests/output_1.txt
-FC my_output2.txt tests/output_2.txt
-FC my_output3.txt tests/output_3.txt
+.\ex1.exe 3 600 < tests\input_1.txt > my_output1c.txt
+.\ex1.exe 7 < tests\input_2.txt > my_output2c.txt
+.\ex1.exe 15 300 < tests\input_3.txt > my_output3c.txt
+
+FC my_output1py.txt tests/output_1.txt
+FC my_output2py.txt tests/output_2.txt
+FC my_output3py.txt tests/output_3.txt
+
+FC my_output1c.txt tests/output_1.txt
+FC my_output2c.txt tests/output_2.txt
+FC my_output3c.txt tests/output_3.txt
 pause
